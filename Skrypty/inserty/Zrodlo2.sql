62321830b22557fa2da66275767a9c80e33a752f
Piotr Filochowski <piotr.filochowski01@gmail.com>
2019-09-13 13:37:20 +0200

    inserty

---
+++ Skrypty/inserty/Zrodlo2.sql
@@ -0,0 +1,13 @@
+Use [Zrodlo2]
+
+select * from Zrodlo2.dbo.BANK;
+
+insert into Zrodlo2.dbo.BANK values
+(1, 'Pekao SA'),
+(2, 'PKO BP'),
+(3, 'BNP BGZ Paribas'),
+(4, 'Santander'),
+(5, 'Millenium');
+
+
+insert into Zrodlo2.dbo.
